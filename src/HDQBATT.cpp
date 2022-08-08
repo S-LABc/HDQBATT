e5e70bee6e8d68de6844be5e4694a570a6996018
Roman <romansklyar15@gmail.com>
2022-08-08 21:30:20 +0300

    Update HDQBATT.cpp

--- src/HDQBATT.cpp
+++ src/HDQBATT.cpp
@@ -29,8 +29,6 @@
  * Проект I2CBATT на GitHub - https://github.com/S-LABc/I2CBATT
  * 
  * Контакты:
- ** YouTube - https://www.youtube.com/channel/UCbkE52YKRphgkvQtdwzQbZQ
- ** Telegram - https://www.t.me/slabyt
  ** GitHub - https://github.com/S-LABc
  ** Gmail - romansklyar15@gmail.com
  * 
